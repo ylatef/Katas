975834307f2485bb33893a920fd0ba6b3119e7b4
Yaser Latef <yaser.latef@gmail.com>
2015-01-20 18:32:52 +0000

    renaming projects and files

---
+++ StringCalculator/StringCalculator/Calculator.cs
@@ -0,0 +1,6 @@
+﻿namespace StringCalculator
+{
+    public class Calculator
+    {
+    }
+}
---
+++ StringCalculator/StringCalculatorTests/CalculateTests.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculatorTests
+{
+    [TestFixture]
+    public class when_calculate_is_called
+    {
+    }
+}
