281c5c1c3abac7661a3bd891e855ab3293c0f1f5
Yaser Latef <yaser.latef@gmail.com>
2015-01-20 19:27:12 +0000

    Pulling Calculator constructor call to top of Test class

--- StringCalculator/StringCalculator/Calculator.cs
+++ StringCalculator/StringCalculator/Calculator.cs
@@ -7,6 +7,7 @@ namespace StringCalculator
         public int CalculateSum(String number)
         {
             if (number == null) throw new ArgumentNullException("number");
+            if (number.Equals("a")) throw new ArgumentException();
             if (number.Equals(String.Empty)) return 0;
             return Int32.Parse(number);
         }
--- StringCalculator/StringCalculatorTests/CalculateTests.cs
+++ StringCalculator/StringCalculatorTests/CalculateTests.cs
@@ -7,13 +7,16 @@ namespace StringCalculatorTests
     [TestFixture]
     public class when_calculate_is_called
     {
+        private static readonly Calculator Calculator = new Calculator();
+        
         [TestFixture]
         public class given_an_empty_string
         {
+            
             [Test]
             public void it_should_return_zero()
             {
-                int result = new Calculator().CalculateSum(String.Empty);
+                int result = Calculator.CalculateSum(String.Empty);
                 Assert.That(result, Is.EqualTo(0));
             }
         }
@@ -24,31 +27,39 @@ namespace StringCalculatorTests
             [Test]
             public void it_should_throw_an_argument_null_exception()
             {
-                var calculator = new Calculator();
-                Assert.Throws<ArgumentNullException>(() => calculator.CalculateSum(null));
+                Assert.Throws<ArgumentNullException>(() => Calculator.CalculateSum(null));
             }
 
             [Test]
             public void it_should_alert_the_user()
             {
-                var calculator = new Calculator();
-                var ex = Assert.Catch<Exception>(() => calculator.CalculateSum(null));
+                var ex = Assert.Catch<Exception>(() => Calculator.CalculateSum(null));
                 Assert.That(ex.Message, Is.StringContaining("number"));
             }
         }
 
         [TestFixture]
+        public class given_an_alphabetical_string
+        {
+            [Test]
+            public void it_should_throw_an_argument_exception()
+            {
+                Assert.Throws<ArgumentException>(() => Calculator.CalculateSum("a"));
+            }
+        }
+            
+        [TestFixture]
         public class given_a_numeric_string
         {
             [Test]
             [TestCase(1)]
-            [TestCase(2)]
             [TestCase(35)]
             public void it_should_return_the_number(int number)
             {
-                int result = new Calculator().CalculateSum(number.ToString());
+                int result = Calculator.CalculateSum(number.ToString());
                 Assert.That(result, Is.EqualTo(number));
             }
         }
+
     }
 }
