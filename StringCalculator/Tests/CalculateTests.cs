954fb0b8905379ab2923fa7044b741b643c28cbd
Yaser Latef <yaser.latef@gmail.com>
2015-01-21 15:56:42 +0000

    Return 0 for empty string

--- StringCalculator/Tests/CalculateTests.cs
+++ StringCalculator/Tests/CalculateTests.cs
@@ -5,8 +5,17 @@ using Console;
 namespace Tests
 {
     [TestFixture]
-    public class when_calculate_is_called
+    public class when_add_is_called
     {
-	
+        [TestFixture]
+        public class given_an_empty_string
+        {
+            [Test]
+            public void it_should_return_zero()
+            {
+                int result = new Calculator().Add(String.Empty);
+                Assert.That(result, Is.EqualTo(0));
+            }
+        }
 	}
 }
