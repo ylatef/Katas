7d7d2ec4658cf21c641f01b5321a28ef713fe65f
Yaser Latef <yaser.latef@gmail.com>
2015-01-21 16:17:10 +0000

    Handle 2/3 results

--- StringCalculator/Console/Calculator.cs
+++ StringCalculator/Console/Calculator.cs
@@ -4,6 +4,21 @@ namespace Console
 {
     public class Calculator
     {
-        
+        public int Add(string numbers)
+        {
+            if (String.IsNullOrEmpty(numbers)) return 0;
+            int index = numbers.IndexOf(",");
+            if (index > -1)
+            {
+                string[] splitNumbers = numbers.Split(new []{','}, StringSplitOptions.None);
+                int result = 0;
+                foreach (string number in splitNumbers)
+                {
+                    result += Int32.Parse(number);
+                }
+                return result;
+            }
+            return Int32.Parse(numbers);
+        }
     }
 }
