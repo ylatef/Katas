bf256bb52aa3330321524d7b41d30d1b78f19d83
Yaser Latef <yaser.latef@gmail.com>
2015-01-21 16:32:17 +0000

    Refactored tests, removed redunant test

--- StringCalculator/Tests/CalculateTests.cs
+++ StringCalculator/Tests/CalculateTests.cs
@@ -1,21 +1,47 @@
 ﻿using System;
+using System.Linq;
+using System.Runtime.ExceptionServices;
 using NUnit.Framework;
 using Console;
 
 namespace Tests
 {
     [TestFixture]
-    public class when_add_is_called
+    public class calculator_should
     {
-        [TestFixture]
-        public class given_an_empty_string
+        [Test]
+        public void return_zero_given_an_empty_string()
         {
-            [Test]
-            public void it_should_return_zero()
-            {
-                int result = new Calculator().Add(String.Empty);
-                Assert.That(result, Is.EqualTo(0));
-            }
+            int result = new Calculator().Add(String.Empty);
+            Assert.That(result, Is.EqualTo(0));
         }
-	}
+        
+        [Test]
+        public void return_the_parsed_value_given_a_numeric_string()
+        {
+            const int number = 1;
+            int result = new Calculator().Add(number.ToString());
+            Assert.That(result, Is.EqualTo(number));
+        }
+
+        [Test]
+        [TestCase(new[] {1, 15, 3, 4})]
+        public void return_the_sum_given_many_numbers(int[] numbers)
+        {
+            int result = new Calculator().Add(NumbersAsString(numbers));
+            Assert.That(result, Is.EqualTo(numbers.Sum()));
+        }
+
+        private string NumbersAsString(int[] numbers)
+        {
+            if (numbers.Length == 0) return String.Empty;
+            string numbersString = numbers[0].ToString();
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                numbersString += "," + numbers[i];
+            }
+
+            return numbersString;
+        }
+    }
 }
