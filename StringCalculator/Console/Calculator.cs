3b7ae5e16a9eb8a400dcc1f83b9f21c56ce6dfe8
Yaser Latef <yaser.latef@gmail.com>
2015-01-27 12:15:03 +0000

    Handle empty and single number cases

--- StringCalculator/Console/Calculator.cs
+++ StringCalculator/Console/Calculator.cs
@@ -1,10 +1,13 @@
 ﻿using System;
-using System.Linq;
 
 namespace Console
 {
     public class Calculator
     {
-        
+        public int Add(string numbers)
+        {
+            if (numbers.Equals(String.Empty)) return 0;
+            return Int32.Parse(numbers);
+        }
     }
 }
--- StringCalculator/Tests/CalculatorTests.cs
+++ StringCalculator/Tests/CalculatorTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using Console;
 using NUnit.Framework;
 
@@ -8,6 +7,20 @@ namespace Tests
     [TestFixture]
     public class add_should
     {
-        
+        [Test]
+        public void return_zero_given_an_empty_string()
+        {
+            int result = new Calculator().Add(String.Empty);
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(35)]
+        public void return_the_number_given_a_numeric_string(int number)
+        {
+            int result = new Calculator().Add(number.ToString()); 
+            Assert.That(result, Is.EqualTo(number));
+        }
     }
 }
